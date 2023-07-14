cabfe197b8429d721d6e34b312e2f79708d4c74e
Don Kackman <dkackman@gmail.com>
2023-07-13 22:40:34 -0500

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { dumpOffers } from "./dump.js";
 
 await dumpOffers({
-  "host": "former",
-  "port": 55400,
-  "key_path": "C:\\Users\\don\\.rchia\\certs\\former\\private_daemon.key",
-  "cert_path": "C:\\Users\\don\\.rchia\\certs\\former\\private_daemon.crt",
-  "timeout_seconds": 30,
-  "fingerprint": 420316971,
+    host: "localhost",
+    port: 55400,
+    key_path: "~\\.chia\\mainnet\\config\\ssl\\daemon\\private_daemon.key",
+    cert_path: "~\\.chia\\mainnet\\config\\ssl\\daemon\\private_daemon.crt",
+    timeout_seconds: 30,
+    fingerprint: 420316971, // fp of your wallet if you have more than 1
 });
-
